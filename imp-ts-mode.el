a1ae11c2a1ae64cf6c3afdbd0bec51d1258f9e5f
Dspil <dennisspiliopoylos@gmail.com>
2023-07-29 01:53:20 +0200

    assignment rule checking

--- imp-ts-mode.el
+++ imp-ts-mode.el
@@ -18,7 +18,6 @@
 
 ;;; Code:
 
-
 ;; ====================
 ;; Indentation and font
 ;; ====================
@@ -62,13 +61,118 @@
        ((parent-is "annotated_skip") parent-bol 0)
        ((parent-is "implication") parent-bol 0)))))
 
+;; ==============
+;; Error handling
+;; ==============
+
+(defun imp-ts-mode:error (start end msg)
+  (message "Error at <%s:%s>: %s" start end msg))
+
+;; ================
+;; Helper functions
+;; ================
+
+(defun imp-ts-mode:add-if-absent (item lst)
+  (if (member item lst)
+      lst
+    (cons item lst)))
+
+(defun imp-ts-mode:merge (lst1 lst2)
+  (while lst1
+    (setq lst2 (imp-ts-mode:add-if-absent (car lst1) lst2))
+    (setq lst1 (cdr lst1)))
+  lst2)
+
 ;; ============
 ;; Verification
 ;; ============
 
+;; z3 query creation
+
+(defun imp-ts-mode:query-terms-from-node (terms node)
+  (pcase (treesit-node-type node)
+    ("lexp" (pcase (treesit-node-type (treesit-node-child node 0))
+              ("(" (imp-ts-mode:query-terms-from-node terms (treesit-node-child node 1)))
+              ("!" (let* ((data (imp-ts-mode:query-terms-from-node terms (treesit-node-child node 1)))
+                          (new-terms (car data))
+                          (query (cdr data)))
+                     (cons new-terms (format "(not %s)" query))))
+              ((or "lexp" "laexp")
+               (let* ((data-left (imp-ts-mode:query-terms-from-node terms (treesit-node-child node 0)))
+                      (new-terms-left (car data-left))
+                      (query-left (cdr data-left))
+                      (data-right (imp-ts-mode:query-terms-from-node terms (treesit-node-child node 2)))
+                      (new-terms-right (car data-right))
+                      (query-right (cdr data-right)))
+                 (cons (imp-ts-mode:merge new-terms-left new-terms-right)
+                       (format "(%s %s %s)"
+                               (let* ((op (treesit-node-child node 1))
+                                      (op-type (treesit-node-type op)))
+                                 (pcase op-type
+                                   ("&&" "and")
+                                   ("||" "or")
+                                   ("rop" (pcase (treesit-node-text op)
+                                            ("#" "neq")
+                                            (rop rop)))))
+                               query-left
+                               query-right))))))
+    ("laexp" (pcase (treesit-node-type (treesit-node-child node 0))
+               ("(" (imp-ts-mode:query-terms-from-node terms (treesit-node-child node 1)))
+               ("-" (let* ((data (imp-ts-mode:query-terms-from-node terms (treesit-node-child node 1)))
+                           (new-terms (car data))
+                           (query (cdr data)))
+                      (cons new-terms (format "(- 0 %s)" query))))
+               ("laexp"
+                (let* ((data-left (imp-ts-mode:query-terms-from-node terms (treesit-node-child node 0)))
+                       (new-terms-left (car data-left))
+                       (query-left (cdr data-left))
+                       (data-right (imp-ts-mode:query-terms-from-node terms (treesit-node-child node 2)))
+                       (new-terms-right (car data-right))
+                       (query-right (cdr data-right)))
+                  (cons (imp-ts-mode:merge new-terms-left new-terms-right)
+                        (format "(%s %s %s)"
+                                (treesit-node-text (treesit-node-child node 1))
+                                query-left
+                                query-right))))
+               ("identifier" (let ((id (treesit-node-text node))) (cons (list id) id)))
+               ("numeral" (cons nil (treesit-node-text node)))))))
+
+(defun imp-ts-mode:data-from-single-condition (single-condition)
+  (imp-ts-mode:query-terms-from-node nil (treesit-node-child single-condition 1)))
+
+(defun imp-ts-mode:query-z3 (terms query)
+  "Return t if the implication holds, nil if it doesn't."
+  (with-temp-buffer
+    (insert "(define-fun neq ((x!0 Int) (x!1 Int)) Bool (not (= x!0 x!1)))\n")
+    (while terms
+      (insert (format "(declare-const %s Int)\n" (car terms)))
+      (setq terms (cdr terms)))
+    (insert (format "(assert (not %s))\n(check-sat)\n" query))
+    (shell-command-on-region (point-min) (point-max) "z3 -in" (current-buffer) t)
+    (let ((z3-ret (string-trim (buffer-string))))
+      (pcase z3-ret
+        ("unsat" t)
+        ("sat" nil)
+        (_ (message "Unknown Z3 answer: %s" z3-ret) nil)))))
+
+;; tree traversal
+
+(defun imp-ts-mode:translate-typ (typ)
+  (if (equal typ "aexp")
+      "laexp"
+    typ))
+
+(defun imp-ts-mode:node-child-no-paren (node)
+  (if (equal (treesit-node-type node) "single_condition")
+      (imp-ts-mode:node-child-no-paren (treesit-node-child node 1))
+    (if (equal (treesit-node-type (treesit-node-child node 0)) "(")
+        (imp-ts-mode:node-child-no-paren (treesit-node-child node 1))
+      node)))
+
 (defun imp-ts-mode:logic-first (node)
   (pcase (treesit-node-type node)
-    ((or "annotated_assignment" "annotated_whilestm" "annotated_seqn" "annotated_skip") (imp-ts-mode:logic-first (treesit-node-child node 0)))
+    ((or "annotated_assignment" "annotated_whilestm" "annotated_seqn" "annotated_skip")
+     (imp-ts-mode:logic-first (treesit-node-child node 0)))
     ("single_condition" node)
     ("implication" (imp-ts-mode:logic-first (treesit-node-child node 0)))))
 
@@ -81,7 +185,25 @@
   (imp-ts-mode:logic-last (treesit-node-child node 0)))
 
 (defun imp-ts-mode:check-logic (node)
-  (message "Checking logic %s" node))
+  (message "Checking logic %s" node)
+  (pcase (treesit-node-type node)
+    ("implication" (let* ((left (treesit-node-child node 0))
+                          (right (treesit-node-child node 2))
+                          (last-left (imp-ts-mode:logic-last left))
+                          (first-right (imp-ts-mode:logic-first right))
+                          (data-left (imp-ts-mode:data-from-single-condition last-left))
+                          (terms-left (car data-left))
+                          (query-left (cdr data-left))
+                          (data-right (imp-ts-mode:data-from-single-condition first-right))
+                          (terms-right (car data-right))
+                          (query-right (cdr data-right))
+                          (terms (imp-ts-mode:merge terms-left terms-right))
+                          (query (format "(=> %s %s)" query-left query-right)))
+                     (imp-ts-mode:check-logic left)
+                     (imp-ts-mode:check-logic right)
+                     (when (not (imp-ts-mode:query-z3 terms query))
+                       (imp-ts-mode:error (treesit-node-start last-left) (treesit-node-end first-right) "Implication might not hold."))))
+    (_ nil)))
 
 (defun imp-ts-mode:check-pre (node)
   (imp-ts-mode:check-logic (treesit-node-child node 0)))
@@ -91,11 +213,52 @@
     (imp-ts-mode:check-logic post)
     (imp-ts-mode:verify-node (treesit-node-child node 0) (imp-ts-mode:logic-first post))))
 
+(defun imp-ts-mode:check-assignment-rule (pre post left right &optional replaced)
+  (let ((pre-typ (treesit-node-type pre))
+        (post-typ (imp-ts-mode:translate-typ (treesit-node-type post))))
+    (when  (or (equal `(,pre-typ . ,post-typ) '("lexp" . "lexp"))
+               (equal `(,pre-typ . ,post-typ) '("laexp" . "laexp")))
+      (let* ((fst-pre (treesit-node-child pre 0))
+             (fst-post (treesit-node-child post 0))
+             (fst-pre-typ (treesit-node-type fst-pre))
+             (fst-post-typ (imp-ts-mode:translate-typ (treesit-node-type fst-post))))
+        (pcase `(,fst-pre-typ . ,fst-post-typ)
+          ((or '("lexp" . "lexp") '("laexp" . "laexp"))
+           (when (equal (treesit-node-text (treesit-node-child pre 1)) (treesit-node-text (treesit-node-child post 1)))
+             (and (imp-ts-mode:check-assignment-rule (imp-ts-mode:node-child-no-paren fst-pre)
+                                                     (imp-ts-mode:node-child-no-paren fst-post)
+                                                     left
+                                                     right
+                                                     replaced)
+                  (imp-ts-mode:check-assignment-rule (imp-ts-mode:node-child-no-paren (treesit-node-child pre 2))
+                                                     (imp-ts-mode:node-child-no-paren (treesit-node-child post 2))
+                                                     left
+                                                     right
+                                                     replaced))))
+          ((or '("!" . "!") '("-" . "-"))
+           (imp-ts-mode:check-assignment-rule (imp-ts-mode:node-child-no-paren (treesit-node-child pre 1))
+                                              (imp-ts-mode:node-child-no-paren (treesit-node-child post 1))
+                                              left
+                                              right
+                                              replace))
+          ((guard (and (equal fst-post-typ "identifier")
+                       (equal left (treesit-node-text fst-post))
+                       (not replaced)))
+           (imp-ts-mode:check-assignment-rule pre right left right t))
+          ((or '("identifier" . "identifier") '("numeral" . "numeral"))
+           (equal (treesit-node-text fst-pre) (treesit-node-text fst-post))))))))
+
 (defun imp-ts-mode:check-assignment (node post)
   (let ((pre (imp-ts-mode:precondition node))
         (left (treesit-node-child node 1))
         (right (treesit-node-child node 3)))
-    (message "checking assignment; {%s} %s := %s {%s}" pre left right post)))
+    (if (not (imp-ts-mode:check-assignment-rule (imp-ts-mode:node-child-no-paren pre)
+                                                (imp-ts-mode:node-child-no-paren post)
+                                                (treesit-node-text left)
+                                                right))
+        (imp-ts-mode:error (treesit-node-start left)
+                           (treesit-node-end right)
+                           "Assignment rule not applied properly."))))
 
 (defun imp-ts-mode:check-whilestm (node post)
   (let* ((pre (imp-ts-mode:precondition node))
